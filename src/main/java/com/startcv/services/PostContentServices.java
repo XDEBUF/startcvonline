9cfc055a5eb2f63ae4a7b267ce09e53ad1f98555
blackswan <x.debuf@gmail.com>
2023-10-14 21:30:01 +0200

    commentaires pour aide ulterieure

--- src/main/java/com/startcv/services/PostContentServices.java
+++ src/main/java/com/startcv/services/PostContentServices.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.startcv.entities.ContentEntity;
+/**
+ * Atomiser les requêtes
+ * et creer des interfaces
+ * pour rassembler les elements à inserer
+ * lire https://www.baeldung.com/spring-data-jpa-query
+ * */
 public class PostContentServices {
 
     @Modifying
